743d467ddf111b41411212b9a6a95e24b2bb6e7e
alexpaco <alexandrepacoret@yahoo.fr>
2017-08-21 17:31:38 +0200

    Mise en place de services pour afficher les personnage ainsi que les détails

--- src/AP/PersonnagesBundle/Controller/AfficheController.php
+++ src/AP/PersonnagesBundle/Controller/AfficheController.php
@@ -8,30 +8,9 @@ class AfficheController extends Controller
 {
 	public function ItemsAction()
 	{	
-		
-		$timestamp = 'date.timestamp()';
-		$publicKey = '1577457f3589885adafd0320c0830897';
-		$privateKey = '5792bfaf981b65942017f8c801d117e8ed795118';
-		$keys = $privateKey.$publicKey;
-		
-		$string = $timestamp.$keys;
-		
-		$md5 = hash('md5', $string);
-		
-		$curl = curl_init("http://gateway.marvel.com:80/v1/public/characters?offset=100&limit=20&ts=$timestamp&apikey=$publicKey&hash=$md5");
-		
-		curl_setopt($curl, CURLOPT_HEADER, 0);
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($curl, CURLOPT_BINARYTRANSFER, true);
-		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE); 
-		
-		$personnages = curl_exec($curl);
-		curl_close($curl);
+		$personnages = $this->container->get('personnages');
 
-		$personnages = json_decode($personnages);
-		// dump($personnages);
-		// die;
 		return $this->render("APPersonnagesBundle:Items:personnages.html.twig", array(
-			'personnages' => $personnages ));
+			'personnages' => $personnages->affiche() ));
 	}
 }
---
+++ src/AP/PersonnagesBundle/Services/Personnages.php
@@ -0,0 +1,33 @@
+<?php 
+	
+namespace AP\PersonnagesBundle\Services;
+
+class Personnages
+{
+	public function Affiche()
+	{
+		$timestamp = 'date.timestamp()';
+		$publicKey = '1577457f3589885adafd0320c0830897';
+		$privateKey = '5792bfaf981b65942017f8c801d117e8ed795118';
+		$keys = $privateKey.$publicKey;
+			
+		$string = $timestamp.$keys;
+			
+		$md5 = hash('md5', $string);
+			
+		$curl = curl_init("http://gateway.marvel.com:80/v1/public/characters?offset=100&limit=20&ts=$timestamp&apikey=$publicKey&hash=$md5");
+			
+		curl_setopt($curl, CURLOPT_HEADER, 0);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_BINARYTRANSFER, true);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE); 
+			
+		$personnages = curl_exec($curl);
+		curl_close($curl);
+
+		$personnages = json_decode($personnages);
+
+		return $personnages;
+	}
+}
+ ?>
